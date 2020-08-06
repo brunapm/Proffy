c112c0f871a66f62232369e8ad8acc8897a02a99
Bruna de P. M <brunamonarin@gmail.com>
2020-08-05 23:24:43 -0300

    Connect to server

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+    subject: string;
+    cost: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/65819100?s=460&u=418b9bd94f4f9bcd2f3494bfd7b3a8ab8fd08662&v=4" alt="Bruna Monarin"/>
+                <img src={teacher.avatar} alt={TeacherItem.name}/>
                 <div>
-                    <strong>Bruna Monarin</strong>
-                    <span>Programação</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta do ensino de programação em todos os níveis.
-                <br /><br />
-                Apaixonada por trazer mais mulheres ao universo da programação.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R${teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="WhatsApp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
